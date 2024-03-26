7e58fc47912d8bac93aa9b51b4d64c2324ed6b17
Plamen Yordanov <plamenyordanov@Plamens-MacBook-Pro.local>
2024-03-27 00:54:06 +0200

    initial

---
+++ init.lua
@@ -0,0 +1,26 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+--term guicolors
+vim.opt.termguicolors = true
+
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.cmp"
+require "user.lsp"
+
+if not vim.g.vscode then
+  require "user.comment"
+  require "user.telescope"
+  require "user.treesitter"
+  require "user.nvimtree"
+  require "user.autopairs"
+  require "user.gitsigns"
+  require "user.bufferlinep"
+
+  vim.cmd "colorscheme tokyonight-night"
+end
+
+
